8e777b1304775cb6fd72caa2d7561308f40a6d55
inweez <115917221+inweez@users.noreply.github.com>
2023-03-19 13:19:30 -0700

    Update scrapeNFL_stats.py

--- scrapeNFL_stats.py
+++ scrapeNFL_stats.py
@@ -3,29 +3,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from csv import reader
 import pprint
-# from yahoo_oauth import OAuth2
-# import yahoo_fantasy_api as yfa
-
-# YAHOO ASS
-# app_id = '5HVeCqGr'
-# client_id = 'dj0yJmk9UzdtT29Wc0VZUUhsJmQ9WVdrOU5VaFdaVU54UjNJbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTRi'
-# client_secret = 'c07d403eed881901c69e09069957216951cdd58a'
-#
-# params = {
-#     "access_token": '',
-#     "consumer_key": client_id,
-#     "consumer_secret": client_secret,
-#     "refresh_token": '',
-#     "token_time": 1433553339.706037,
-#     "token_type": "bearer"
-# }
-#
-# sc = OAuth2(None, None, from_file='oauth2.json')
-
-
-
-# pro football - barely worked on!!!
-
 
 year = input('What year?')
 player_name = input('What player?').title().replace('*', '', 1).replace('+', '', 1)
